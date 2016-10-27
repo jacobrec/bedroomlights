db62af02910c5861f159f922bd591fc2191e2496
Jacob Reckhard <jacobrec@live.com>
2016-10-27 08:31:19 -0600

    added quote

--- www/lights.js
+++ www/lights.js
@@ -26,6 +26,16 @@ ws.onmessage = function(evt) {
 
 
 $(document).ready(function() {
+  $.ajax({
+      url: "http://api.forismatic.com/api/1.0/?",
+      dataType: "jsonp",
+      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+      success: function( response ) {
+        $("#quotebox").html("<p id='random_quote' class='lead text-center'>" +
+          response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
+
+      }
+  });
 
     $("#sub").click(function() {
         if ($("#passwd").val() != "") {
